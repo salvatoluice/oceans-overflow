f8c9a50d8347ad41b789377c5d511fef013d0b36
Salvato Luice <salvatoluice5@gmai.com>
2022-12-31 17:53:42 +0300

    Add navabar

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,11 +1,45 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import styles from './Header.module.scss';
+import { FaShoppingCart } from 'react-icons/fa'
+
+const logo = (
+  <div className={styles.logo}>
+    <Link to='/'>
+      <h2>
+        Oceans
+      </h2>
+    </Link>
+  </div>
+);
 
 const Header = () => {
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
+    <header>
+      <div className={styles.header}>
+        {logo}
+        <nav>
+          <ul>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/contact'>Contacts</Link>
+            </li>
+          </ul>
+        </nav>
+        <div className={styles["header-right"]}>
+          <span className={styles.links}>
+            <Link to='/login'>Login</Link>
+            <Link to='/register'>Register</Link>
+            <Link to='/order-history'>Orders</Link>
+          </span>
+          <span className={styles.cart}>
+            <Link to='/cart'>Cart <FaShoppingCart size={20} /><p>0</p> </Link>
+          </span>
+        </div>
+      </div>
+    </header>
   );
 };
 
