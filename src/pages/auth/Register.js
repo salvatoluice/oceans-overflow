fec543693204266fbd401a868f07b38e7f69bc77
Salvato Luice <salvatoluice5@gmai.com>
2023-01-01 00:51:52 +0300

    Auth layout

--- src/pages/auth/Register.js
+++ src/pages/auth/Register.js
@@ -1,8 +1,31 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Card from '../../components/card/Card';
+import styles from './auth.module.scss';
+import register from '../../assets/register.png';
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <section className={`container ${styles.auth}`}>
+    <Card>
+    <div className={styles.form}>
+      <h2>Register</h2>
+        <form>
+          <input type='text' placeholder='Email' required/>
+          <input type='password' placeholder='Password' required/>
+          <input type='password' placeholder='Confirm Password' required/>
+          <button className='--btn --btn-primary --btn-block'>Register</button>
+      </form>
+      <span className={styles.register}>
+        <p>Already have an account? </p>
+        <Link to='/login'>Login</Link>
+      </span>
+    </div>
+    </Card>
+    <div className={styles.img}>
+      <img width='400' src={register} alt="register" />
+    </div>
+  </section>
   )
 }
 
--- src/pages/auth/Reset.js
+++ src/pages/auth/Reset.js
@@ -1,8 +1,34 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import reset from '../../assets/forgot.png'
+import Card from '../../components/card/Card'
+import styles from './auth.module.scss'
 
 const Reset = () => {
   return (
-    <div>Reset</div>
+    <section className={`container ${styles.auth}`}>
+    <div className={styles.img}>
+      <img width='400' src={reset} alt="Reset" />
+    </div>
+    <Card>
+    <div className={styles.form}>
+      <h2>Reset Password</h2>
+        <form>
+          <input type='text' placeholder='Email' required/>
+          <button className='--btn --btn-primary --btn-block'>Reset Password</button>
+          <div className={styles.links}>
+            <p>
+              <Link to='/login'>Login</Link>
+            </p>
+            <p>
+              <Link to='/register'>Register</Link>
+            </p>
+          </div>
+      </form>
+
+    </div>
+    </Card>
+  </section>
   )
 }
 
